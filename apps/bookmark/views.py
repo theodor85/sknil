40f9e1156dcaabf2ac894ca718e9d2701934f973
Fedor Kosolapov <fedor_coder@mail.ru>
2020-07-14 22:29:38 +0700

    lesson 4

---
+++ apps/bookmark/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def categories(request):
+    return render(request, 'bookmark/categories.html')
