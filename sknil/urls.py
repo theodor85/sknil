0866cf83be7ea10673cb3c7fe3a8f99e68ce253a
Fedor Kosolapov <fedor_coder@mail.ru>
2020-07-14 19:47:38 +0700

    lesson 3

--- sknil/urls.py
+++ sknil/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path
 
-from apps.core.views import frontpage
+from apps.core.views import frontpage, signup
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('signup/', signup, name='signup'),
     path('', frontpage, name='frontpage'),
 ]
